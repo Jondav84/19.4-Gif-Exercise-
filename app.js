663f0632b9be74890e846cf81d40cb727041d370
Jondav84 <121706134+Jondav84@users.noreply.github.com>
2023-05-26 23:12:07 -0700

    Fixes to the js and  adding a little css

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /** @format */
-
+$(document).on("ready", () => {
+  loadGifsFromStorage();
+});
 console.log("Let's get this party started!");
 
 function renderGif(res) {
@@ -7,9 +9,16 @@ function renderGif(res) {
   if (numGifs) {
     let randIdx = Math.floor(Math.random() * numGifs);
     $("#gifContainer").append(
-      $("<div>")
-        .addClass("col-md-4 mb-4")
-        .append($("<img>").attr("src", res.data.data[randIdx].url))
+      $("<div>", { class: "col-6 col-md-4 col-lg-3" }).append(
+        $("<div>")
+          .addClass("img-container")
+          .append(
+            $("<img>", { class: "img-fluid" }).attr(
+              "src",
+              res.data.data[randIdx].images.original.url
+            )
+          )
+      )
     );
   }
 }
@@ -23,11 +32,38 @@ $("#search").on("click", async function (e) {
       q: inputVal,
     },
   });
-  console.log(res.data.data[3].url);
+  console.log(res.data.data[3].images.original.url);
   $("#input").val("");
   renderGif(res);
+  saveGifsToLocalStorage();
 });
 
 $("#remove").on("click", () => {
   $("#gifContainer").empty();
+  localStorage.removeItem("savedgifs");
 });
+
+function saveGifsToLocalStorage() {
+  const gifUrls = [];
+  $("#gifContainer")
+    .find("img")
+    .each(function () {
+      gifUrls.push($(this).attr("src"));
+    });
+  localStorage.setItem("savedgifs", JSON.stringify(gifUrls));
+}
+
+function loadGifsFromStorage() {
+  const savedGifs = JSON.parse(localStorage.getItem("savedgifs"));
+  if (savedGifs) {
+    savedGifs.forEach(function (gifUrl) {
+      $("#gifContainer").append(
+        $("<div>", { class: "col-6 col-md-4 col-lg-3" }).append(
+          $("<div>")
+            .addClass("img-container")
+            .append($("<img>", { class: "img-fluid" }).attr("src", gifUrl))
+        )
+      );
+    });
+  }
+}
